6e7b176ccf48b79966e67b152eb08cf2379f5ba9
Jchench <jessiechen2026@u.northwestern.edu>
2023-10-29 22:18:06 -0500

    finished with memo 1

--- Chen_Jessie_Final_Project.R
+++ Chen_Jessie_Final_Project.R
@@ -1,1 +1,11 @@
 # Loading data and packages
+library(tidyverse)
+library(naniar)
+
+spotify_data_top <- 
+  read_csv("data/ATop 1990-2022.csv")
+
+spotify_streams <- 
+  read_csv("data/spotify_streams.csv")
+
+glimpse(spotify_data_top)
